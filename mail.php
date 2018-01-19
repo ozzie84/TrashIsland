cfff56bfa70fbfd98d7a587d1b8f572b658b87db
ozzie84 <terehov84@live.com>
2018-01-19 12:58:17 +0200

    island trash final-1

--- mail.php
+++ mail.php
@@ -1,59 +1,20 @@
-<html>
-
-<head>
-  <meta charset='utf-8'>
-  <meta http-equiv="refresh" content="5;https://ozzie84.github.io/TrashIsland/index.html/">
-</head>
-
-<body>
 <?php
-error_reporting(0);
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$textarea = trim($_POST["message"]);
+  $recepient = "terehov84@gmail.com";
+  $sitename = "https://ozzie84.github.io/TrashIsland/index.html";
 
-function clean($value = ""){
-  $value = trim($value);
-  $value = strip_tags($value);
+  $headers = 'MIME-Version: 1.0' . "\r\n";
+  $headers = 'Content-type: text/html; charset=iso-8859-1' . "\r\n"
 
-  return $value;
-}
+  $name = trim($_POST["name"]);
+  $phone = trim($_POST["phone"]);
+  $email = trim($_POST["email"]);
+  $textarea = trim($_POST["message"]);
 
-function check_length($value = "", $min, $max) {
-  $result = (mb_strlen($value) < $min || mb_strlen($value) > $max);
-  return !$result;
-}
+  $message = "Name: $name \nPhone Number: $phone \nEmail: $email \nMessage: $textarea";
 
-$name = clean($name);
-$email = clean($email);
-$phone = clean($phone);
-$textarea = clean($message);
+  $pagetitle = "New application from the site \"sitename\"";
 
-if($name && $phone && $email && $textarea) {
-  $email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
-  if(check_length($car_name, 2, 60) && check_length($name, 2, 40) && check_length($phone, 2, 50) && check_length($date, 2, 1000) && $email_validate) {
-    $mail = "terehov84@gmail.com";
-    $message = "<h2>Сообщение с сайта \"Car Rent\":
-    ".$_POST['subj']." (Заказ ".$_POST['form'].")</h2><hr>
-    <p><strong>Имя:</strong> ".$name."</p>
-    <p><strong>E-mail:</strong> ".$email."</p>
-    <p><strong>Телефон:</strong> ".$phone."</p>
-    <p><strong>Дата:</strong> ".message."</p>";
-    $subject="Сообщение с сайта \"Car Rent\"";
-    mail($mail, $subject, $message, "Content-type: text/html; charset=utf-8 \r\n");
-    echo "<h2>Спасибо за Ваш заказ. Скоро с Вами свяжется наш менеджер. </h2> <br>";
-    echo "Вы будете автоматически перенаправлены на главную страницу.";
-  }else{
-    echo "Вы ввели не корректные данные, пожалуйсна повторите попытку.";
-  }
-} else {
-  echo "Пожалуйста заполните пустые поля";
-}
+  mail($reception, $pagetitle, $message, $headers);
+
 ?>
-
-</body>
-
-</html>
-
